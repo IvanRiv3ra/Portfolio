d3d93230375e80e99194b5b2dd4a343aed0ddc78
Ivan Rivera <riveraivan598@gmail.com>
2026-07-22 21:19:52 -0600

    refactor: projects component

---
+++ js/data/projectCardData.js
@@ -0,0 +1,14 @@
+export const projectCardData = [
+  {
+    date: "2025",
+    title: "Zentrego",
+    description:
+      "Descripción genérica provisional en lo que hago una buena para la API Descripción genérica provisional en lo que hago una buena para la API",
+    features: [
+      "Sistema de autenticación con JWT y roles",
+      "Panel de administración en tiempo real",
+      "Tiempo de carga reducido de 4s a 900ms",
+    ],
+    icons: ["icon-net", "icon-sqlserver", "icon-git", "icon-jwt"],
+  },
+];
